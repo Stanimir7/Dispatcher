efba295afcd218cec620228e837db084262fc69d
Justin Gavin <justintgav@gmail.com>
2017-10-31 13:41:55 -0400

    Completed unique URL generation/page for drivers for each job. Started on confirmation page /claim_job.

--- bin/__init__.py
+++ bin/__init__.py
@@ -20,8 +20,6 @@ mysql.init_app(app)
 @app.route("/create_job", methods=['POST','GET'])
 def create_job():
     try:
-        #TODO grab info from post request here, throw into correct vars
-        #merchant ID is the only thing that is required, rest just pass in empty string if you don't want to worry about it for now
         #_m=hashlib.md5()
         body=""
         _merch_id = request.get_json().get('merch_id','')
@@ -40,7 +38,6 @@ def create_job():
             mysql.connection.rollback()
             return jsonify({'status': 'error','message': 'Empty DB Response'})
         mysql.connection.commit()
-        #cursor.execute("SELECT BusName FROM Business WHERE MerchantID = "+_merch_id)
         _bus_name=data[0].get('BusName')
         body="New Job from "+ _bus_name
         #_m.update(_merch_id+_job_title+_job_desc+_from_loc+_to_loc+_bus_phone)
@@ -84,8 +81,8 @@ def create_job():
             #_job_hash=m.hexdigest() % 10**8
             #############################
             
-            #body_link=url_for('claim_page',hashed_value=unique_url)
-            body_link = unique_url
+            body_link=url_for('claim_page',unique_url=unique_url)
+            #body_link = unique_url
             body=body+" Claim link: "+body_link
             if do_sms:
                 sms.send_sms.send(num, body)
@@ -93,39 +90,87 @@ def create_job():
             res = res + '|' + body
     except Exception as e:
         return jsonify({'status':str(e)})
-    #return str(request.form)
-    return res #should never get here
+    return jsonify({'status':'success'})
+    #return res #should never get here
 
-#@app.route("/claim_page/<hashed_value>",methods=["POST","GET"])
-#def claim_page(hashed_value):
-#    conn = mysql.connect()
-#    cursor = conn.cursor()
-#    
-#    cursor.execute("SELECT jobID FROM <TableName> WHERE <uniqueJobID> = "+hashed_value)
-#    _job_id=cursor.fetchone()
-#    cursor.execute("SELECT job_status FROM <TableName> Where <uniqueJobID> = "+_job_id)
-#    _job_status=cursor.fetchone()
-#    if(_job_status == "open")
-#        cursor.execute("SELECT driverID FROM <TableName> WHERE <uniqueJobID> = "+hashed_value)
-#        _driver = cursor.fetchone()    
-#        # db call change _job_status and assign driver 
-#        cursor.close()
-#        conn.close()
-#        return render_template('claim.html',_bus_name=_bus_name,_job_title=_jobtitle,_job_desc=_job_desc,_from_loc=_from_loc,_to_loc=_to_loc,_bus_phone=bus_phone)
-#    
-#    else
-#        cursor.execute("SELECT driverID FROM <TableName> WHERE <uniqueJobID> = "+hashed_value)
-#        _driver_current_accessor = cursor.fetchone()    
-#        
-#        cursor.execute("SELECT driverID FROM <TableName> WHERE <JobID> = "+_job_id)
-#        _driver_actual = cursor.fetchone()    
-#        if(_driver_current_accessor==_driver_actual)
-#            ## go to endpoint for canceling and completing a job
-#            cursor.close()
-#            conn.close()
-#            return render_template('can_comp.html')
-#        cursor.close()
-#        conn.close()
-#        return render_template('taken.html',...) #add aditional info
+@app.route("/claim_page/<unique_url>",methods=["POST","GET"])
+def claim_page(unique_url):
+    #TODO: have this page be a manage job thing too, not just claim button
+    #other than that, this is working
+    try:
+        cursor = mysql.connection.cursor()
+        cursor.callproc('get_job_driver_from_url',[unique_url])
+        data_url = cursor.fetchall()
+        cursor.close()
+        if len(data_url) is 0:
+            mysql.connection.rollback()
+            return render_template('message.html',
+                           title='Whoops',
+                           message='Something went wrong, please try again.')
+        elif data_url[0].get('status') == 'error':
+            mysql.connection.rollback()
+            return render_template('message.html',
+                           title='Whoops',
+                           message='Something went wrong, please try again.' + data_url[0].get('message'))
+        #Assume success if not error at this point
+        #elif data_url[0].get('status') == 'success':
+        mysql.connection.commit()
+        
+        cursor = mysql.connection.cursor()
+        cursor.callproc('job_avail',[data_url[0].get('idJob')])
+        data = cursor.fetchall()
+        cursor.close()
+        if data[0].get('avail') == 'false':
+            return render_template('message.html',
+                           title='Job Taken',
+                           message='Sorry, this job has been claimed. ')
+        elif data[0].get('avail') == 'true':    
+            return render_template('claim.html',
+                           title='Claim Job',
+                           bus_name=data_url[0].get('BusName'),
+                           job_title=data_url[0].get('JobTitle'),
+                           job_desc=data_url[0].get('JobDesc'),
+                           from_loc=data_url[0].get('FromLoc'),
+                           to_loc=data_url[0].get('ToLoc'),
+                           bus_phone=data_url[0].get('BusContactPhone'),
+                           #Driver's identifying info
+                           unique_url=unique_url
+                           
+                           )
+        
+    except Exception as e:
+        return jsonify({'status':str(e)})
+        #return render_template('message.html', title='Whoops', message='Something went wrong, please try again.')
+    return 'end' #should never get here
+    
+@app.route("/claim_job",methods=["POST","GET"])
+def claim_job():
+    #TODO this is not complete
+    #if not request.form.get('submit')
+        #return
+    unique_url = request.form.get('unique_url')
+    
+    cursor = mysql.connection.cursor()
+    cursor.callproc('get_job_driver_from_url',[unique_url])
+    data_url = cursor.fetchall()
+    cursor.close()
+    if len(data_url) is 0:
+        mysql.connection.rollback()
+        return render_template('message.html',
+                       title='Whoops',
+                       message='Something went wrong, please try again.')
+    elif data_url[0].get('status') == 'error':
+        mysql.connection.rollback()
+        return render_template('message.html',
+                       title='Whoops',
+                       message='Something went wrong, please try again.' + data_url[0].get('message'))
+    #Assume success if not error at this point
+    #elif data_url[0].get('status') == 'success':
+    mysql.connection.commit()
+    
+    #you now have all info through data_url
+    
+    
+    return str(data_url)
     
 
--- bin/test.py
+++
@@ -1,3 +0,0 @@
-def myProc():
-	print("run")
-
