d13dc78b793e431d5a74f19dd68544a2e5dd3c5a
sfitz95 <SeanTFitz95@gmail.com>
2017-11-01 15:41:01 -0400

    Updated to fit with new database changes

--- bin/__init__.py
+++ bin/__init__.py
@@ -17,8 +17,6 @@ app.config['MYSQL_DATABASE_PASSWORD'] = 'dispatcher'
 app.config['MYSQL_DATABASE_DB'] = 'dispatcher'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
-conn = mysql.connect()
-cursor = conn.cursor()
 
 
 @app.route("/")
@@ -73,20 +71,23 @@ def register_business():
 	_phoneNum=  request.get_json().get('phone_num','')
 	_address=  request.get_json().get('merch_address','')
 	
+	cursor = mysql.connection.cursor()
 	#call database stored proc
 	#CALL `dispatcher`.`new_business`(<{IN p_merch_id CHAR(32)}>, <{IN p_name VARCHAR(128)}>, <{IN p_address VARCHAR(256)}>, <{IN p_phone CHAR(15)}>);
 	cursor.callproc('new_business',(_merchID,_merchName,_address,_phoneNum))
 	
 	data = cursor.fetchall()
+	cursor.close()
  
 	if len(data) is 0:
-		return jsonify({'status':"ERROR: Empty Response"})
+		mysql.connection.rollback()
+		return jsonify({'status':'error','message' ,"ERROR: Empty Response"})
 	if data[0] is 'error':
+		mysql.connection.rollback()
 		return jsonify({'status':'error: ' + str(data[1])})
 	
 	#commit changes to DB
-	conn.commit()
-	
+	mysql.connection.commit()
 	return jsonify({'status':"success"})
 	
 @app.route("/receive_text", methods=['Get', 'Post'])
