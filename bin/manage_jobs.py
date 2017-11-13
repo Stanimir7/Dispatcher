71e3fdabb264d39270569f65114dc0b953cace89
sfitz95 <SeanTFitz95@gmail.com>
2017-11-12 21:15:38 -0500

    Update both functions
    Fully fleshed out to handle both canceled and completed jobs.

--- bin/manage_jobs.py
+++ bin/manage_jobs.py
@@ -7,18 +7,55 @@ from bin import app, mysql
 @app.route("/cancel_job", methods=['POST'])
 def cancel_job():
   
-   cursor = mysql.connection.cursor()
-   #Will need to access database to remove job from list
-   
-   data = cursor.fetchall()
-   cursor.close()
-
-@app.route("/completed_job", methods=['POST'])
-def completed_job():
+  _driver_id =  request.get_json().get('driver_id','')
+  _job_id =  request.get_json().get('job_name','')
 
   cursor = mysql.connection.cursor()
-  #Will need to access database to add job to complete list(or change Status, which ever way we handle)
+  
+  #CALL `dispatcher`.`driver_close_job`(<{IN p_idDriver INT}>, <{IN p_idJob INT}>, <{IN p_status ENUM('complete', 'canceled')}>);	
+  cursor.callproc('driver_close_job', [driver_id, job_id, ENUM('canceled')])
   
   data = cursor.fetchall()
   cursor.close()
   
+  #Check for Error is DB call
+  if len(data) is 0:
+        mysql.connection.rollback()
+        return jsonify({'status':'error','message': "ERROR: Empty Response"})
+  if data[0].get('status') == 'error':
+        #TODO: display the meaningful error:
+        #data[0].get('message')
+        mysql.connection.rollback()
+        return jsonify({'status':'error: ' + str(data[0].get('message'))})
+    
+   #commit changes to DB
+   mysql.connection.commit()
+   return jsonify({'status':"success"}
+
+@app.route("/completed_job", methods=['POST'])
+def completed_job():
+  
+  _driver_id =  request.get_json().get('driver_id','')
+  _job_id =  request.get_json().get('job_name','')
+
+  cursor = mysql.connection.cursor()
+  
+  #CALL `dispatcher`.`driver_close_job`(<{IN p_idDriver INT}>, <{IN p_idJob INT}>, <{IN p_status ENUM('complete', 'canceled')}>);	
+  cursor.callproc('driver_close_job', [driver_id, job_id, ENUM('complete')])
+  
+  data = cursor.fetchall()
+  cursor.close()
+  
+  #Check for Error is DB call
+  if len(data) is 0:
+        mysql.connection.rollback()
+        return jsonify({'status':'error','message': "ERROR: Empty Response"})
+  if data[0].get('status') == 'error':
+        #TODO: display the meaningful error:
+        #data[0].get('message')
+        mysql.connection.rollback()
+        return jsonify({'status':'error: ' + str(data[0].get('message'))})
+    
+   #commit changes to DB
+   mysql.connection.commit()
+   return jsonify({'status':"success"})
