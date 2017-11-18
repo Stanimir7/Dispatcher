4fa884f0b1f6cbc79ef78271398093037571cb73
Stanimir7 <genovs6@students.rowan.edu>
2017-11-18 11:03:08 -0500

    Update driver_job_tasks.py
    Got rid of redundant wrong code.

--- bin/driver_job_tasks.py
+++ bin/driver_job_tasks.py
@@ -133,6 +133,9 @@ def claim_job():
     #should never get here
     return str(data_url)
 
+############################
+######## Driver Cancel and Complete Job Action ########
+############################
 @app.route("/driver_close", methods=["POST","GET"])
 def driver_close():
     unique_url = request.form.get('unique_url')
@@ -237,65 +240,4 @@ def driver_close():
         return render_template('message.html',
                        title='Whoops',
                        message='You are not the driver that has claimed this job, how did you get here?')  
-    
-
-
-############################
-######## Redundant Stuff????? ########
-############################
-#@app.route("/driver_cancel_job", methods=['POST'])
-#def driver_cancel_job():
-#    _driver_id =  request.get_json().get('driver_id','')
-#    _job_id =  request.get_json().get('job_name','')
-#    
-#    cursor = mysql.connection.cursor()
-#    
-#    #CALL `dispatcher`.`driver_close_job`(<{IN p_idDriver INT}>, <{IN p_idJob INT}>, <{IN p_status ENUM('complete', 'canceled')}>);    
-#    cursor.callproc('driver_close_job', [driver_id, job_id, 'canceled'])
-#    
-#    data = cursor.fetchall()
-#    cursor.close()
-#    
-#    #Check for Error is DB call
-#    if len(data) is 0:
-#        mysql.connection.rollback()
-#        return jsonify({'status':'error','message': "ERROR: Empty Response"})
-#    if data[0].get('status') == 'error':
-#        #TODO: display the meaningful error:
-#        #data[0].get('message')
-#        mysql.connection.rollback()
-#        return jsonify({'status':'error: ' + str(data[0].get('message'))})
-#
-#    #commit changes to DB
-#    mysql.connection.commit()
-#    return jsonify({'status':"success"})
-#
-#@app.route("/driver_completed_job", methods=['POST'])
-#def driver_completed_job():
-#    _driver_id =  request.get_json().get('driver_id','')
-#    _job_id =  request.get_json().get('job_name','')
-#    
-#    cursor = mysql.connection.cursor()
-#    
-#    #CALL `dispatcher`.`driver_close_job`(<{IN p_idDriver INT}>, <{IN p_idJob INT}>, <{IN p_status ENUM('complete', 'canceled')}>);    
-#    cursor.callproc('driver_close_job', [driver_id, job_id, 'complete'])
-#    
-#    data = cursor.fetchall()
-#    cursor.close()
-#    
-#    #Check for Error is DB call
-#    if len(data) is 0:
-#        mysql.connection.rollback()
-#        return jsonify({'status':'error','message': "ERROR: Empty Response"})
-#    if data[0].get('status') == 'error':
-#        #TODO: display the meaningful error:
-#        #data[0].get('message')
-#        mysql.connection.rollback()
-#        return jsonify({'status':'error: ' + str(data[0].get('message'))})
-#    
-#    #commit changes to DB
-#    mysql.connection.commit()
-#    return jsonify({'status':"success"})
-#
-#
-
+   
